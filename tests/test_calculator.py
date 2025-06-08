76dd549f12d7fa633129d95edf7da866b2914260
jaydrava <jraval.0901@gmail.com>
2025-06-08 02:05:21 -0400

    Add test for add function with negative numbers

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,6 +6,10 @@ def test_add():
     assert calculate("add", 2, 3) == 5
 
 
+def test_add_negative():
+    assert calculate("add", -2, -3) == -5
+
+
 def test_subtract():
     assert calculate("subtract", 5, 3) == 2
 
